a139f76a51c3a777fff1d996ff27231d2ef8a8f3
nandesh-dev <nandesh.dev@gmail.com>
2024-11-24 17:45:15 +0000

    Fix api url in frontend

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -47,7 +47,7 @@ const router = createBrowserRouter([
 ])
 
 const transport = createGrpcWebTransport({
-    baseUrl: `http://localhost:3000`,
+    baseUrl: `${window.location.origin}`,
 })
 
 const proto: ProtoContent = {
