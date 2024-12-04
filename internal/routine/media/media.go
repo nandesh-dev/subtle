3e50e7048de48ee3cbce20f073f045062b68e5e7
nandesh-dev <nandesh.dev@gmail.com>
2024-12-04 12:05:27 +0000

    Switch to using raw sql for media routine

--- internal/routine/media/media.go
+++ internal/routine/media/media.go
@@ -34,11 +34,10 @@ func Run() {
 			for _, video := range directory.Videos {
 				var count int64
 				if err := database.Database().
-					Model(&database.Video{}).
-					Where(database.Video{
-						DirectoryPath: video.DirectoryPath(),
-						Filename:      video.Filename(),
-					}).Count(&count).Error; err != nil {
+					Raw(
+						"SELECT COUNT(*) FROM videos WHERE directory_path = ? AND filename = ?;",
+						video.DirectoryPath(), video.Filename(),
+					).Scan(&count).Error; err != nil {
 					logger.Error("cannot cound videos in database", "err", err)
 					continue
 				}
@@ -49,11 +48,14 @@ func Run() {
 
 				logger.Info("processing video", slog.Group("info", "path", video.Filepath()))
 
-				videoEntry := database.Video{
-					DirectoryPath: video.DirectoryPath(),
-					Filename:      video.Filename(),
-
-					Subtitles: make([]database.Subtitle, 0),
+				var videoId int64
+				if err := database.Database().
+					Raw(
+						"INSERT INTO videos (directory_path, filename, is_processing) VALUES (?, ?, ?) RETURNING id;",
+						video.DirectoryPath(), video.Filename(), true,
+					).Scan(&videoId).Error; err != nil {
+					logger.Error("cannot insert video into database", "err", err)
+					continue
 				}
 
 				rawStreams, err := video.RawStreams()
@@ -64,25 +66,26 @@ func Run() {
 
 				for _, rawStream := range *rawStreams {
 					title := rawStream.Title()
-
 					if title == "" {
 						title = fmt.Sprintf("%v#%v", display.Self.Name(rawStream.Language()), rawStream.Index())
 					}
+					lang := rawStream.Language()
 
-					subtitleEntry := database.Subtitle{
-						Language: rawStream.Language().String(),
-
-						ImportIsExternal:       false,
-						ImportFormat:           subtitle.MapFormat(rawStream.Format()),
-						ImportVideoStreamIndex: rawStream.Index(),
+					if err := database.Database().
+						Exec(
+							"INSERT INTO subtitles (video_id, language, import_is_external, import_format, import_video_stream_index) VALUES (?, ?, ?, ?, ?);",
+							videoId, lang.String(), false, subtitle.MapFormat(rawStream.Format()), rawStream.Index(),
+						).Error; err != nil {
+						logger.Error("cannot insert subtitle into database", "err", err)
 					}
-
-					videoEntry.Subtitles = append(videoEntry.Subtitles, subtitleEntry)
 				}
 
-				logger.Info("adding video to database", slog.Group("info", "filepath", video.Filepath()))
-				if err := database.Database().Create(&videoEntry).Error; err != nil {
-					logger.Error("cannot save video to database", "err", err)
+				if err := database.Database().
+					Exec(
+						"UPDATE videos SET is_processing=? WHERE id = ?;",
+						false, videoId,
+					).Error; err != nil {
+					logger.Error("cannot update video processing status in database", "err", err)
 				}
 			}
 		}
