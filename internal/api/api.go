77c34f582658d543d1c2e7c513dce36e74b975b0
nandesh-dev <nandesh.dev@gmail.com>
2025-02-15 13:49:03 +0000

    Embed frontend files into application binary

--- internal/api/api.go
+++ internal/api/api.go
@@ -2,9 +2,11 @@ package api
 
 import (
 	"net/http"
+	"path/filepath"
 
 	connectcors "connectrpc.com/cors"
 	"connectrpc.com/grpcreflect"
+	"github.com/nandesh-dev/subtle/generated/embed"
 	"github.com/nandesh-dev/subtle/generated/proto/web/webconnect"
 	"github.com/nandesh-dev/subtle/pkgs/configuration"
 	"github.com/rs/cors"
@@ -36,6 +38,23 @@ func NewAPIServer(configFile *configuration.File, options APIServerOptions) *API
 		path, handler = grpcreflect.NewHandlerV1Alpha(grpcreflect.NewStaticReflector(webconnect.WebServiceName))
 		mux.Handle(path, handler)
 	}
+
+	frontendFilesystem := embed.GetFrontendFilesystem()
+	frontendFileServer := http.FileServer(http.FS(frontendFilesystem))
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		handler, handlerPattern := mux.Handler(r)
+		if handlerPattern != "" && handlerPattern != "/" {
+			handler.ServeHTTP(w, r)
+			return
+		}
+
+		if filepath.Ext(r.URL.Path) == "" {
+			http.ServeFileFS(w, r, frontendFilesystem, "index.html")
+      return
+		}
+
+		frontendFileServer.ServeHTTP(w, r)
+	})
 
 	corsMiddleware := cors.New(cors.Options{
 		AllowedOrigins: options.COROrigins,
